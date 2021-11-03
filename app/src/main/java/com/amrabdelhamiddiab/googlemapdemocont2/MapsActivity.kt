2bf7062abece963cd8533b1ec7b3e3f5b16960c6
engam <engamrdiab52@gmail.com>
2021-11-03 16:14:36 +0200

    24-introducing_to_polyline

--- app/src/main/java/com/amrabdelhamiddiab/googlemapdemocont2/MapsActivity.kt
+++ app/src/main/java/com/amrabdelhamiddiab/googlemapdemocont2/MapsActivity.kt
@@ -1,7 +1,10 @@
 package com.amrabdelhamiddiab.googlemapdemocont2
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,11 +13,23 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.amrabdelhamiddiab.googlemapdemocont2.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnPolylineClickListener {
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private val alexandria = LatLng(31.20588470349843, 29.91779871421426)
+    private val cairo = LatLng(30.04640852760722, 31.236832732735163)
+    private val mansoura = LatLng(31.04343698358825, 31.378033567808494)
+    private val siwa = LatLng(29.280285360336215, 25.5088921201458)
+    private val cyprus = LatLng(34.760781889258894, 32.626692560378494)
+    private val lebanon = LatLng(34.052888370057616, 35.84241239159556)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,21 +43,40 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val alexandria = LatLng(31.20588470349843, 29.91779871421426)
+        map.addMarker(MarkerOptions().position(alexandria).title("Marker in Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(alexandria, 6f))
+        map.uiSettings.apply {
+            isZoomControlsEnabled = true
+        }
+        lifecycleScope.launch {
+            addPolyline()
+        }
+        map.setOnPolylineClickListener(this)
+    }
+
+    private suspend fun addPolyline() {
+        val polyline = map.addPolyline(
+            PolylineOptions().apply {
+                add(alexandria, cairo, mansoura)
+                width(5f)
+                color(Color.BLUE)
+                geodesic(true)
+                clickable(true)
+            }
+        )
+        delay(3000)
+        val newLocationsList = listOf<LatLng>(
+            alexandria, cyprus, lebanon
+        )
+        polyline.points = newLocationsList
+    }
+
+    override fun onPolylineClick(p0: Polyline) {
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
     }
 }
